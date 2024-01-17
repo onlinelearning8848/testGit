cc9be36efe7f1fceb46208cd3852f29df24509b3
onlinelearning <onlinelearning8848@gmail.com>
2024-01-17 07:48:46 -0600

    Test boss

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "yes";
+		if (hungry == "yes")
+			System.out.println("boss eat something");
+		else {
+		System.out.println("Go to bed ");
+		}
+
+	}
+
+}
